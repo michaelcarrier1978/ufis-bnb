cfffe6206db160864195d90014f3c5581782cf84
Michael Carrier <michaelcarrier@Michaels-MBP.hsd1.ut.comcast.net>
2021-07-04 12:08:18 -0600

    added review migration

---
+++ database/migrations/2021_07_04_180409_create_review_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReviewTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('review', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('property_id')->unsigned();
+            $table->integer('rating');
+            $table->string('comment');
+            $table->timestamps();
+            $table->foreign('property_id')->references('property_id')->on('property')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('review');
+    }
+}
